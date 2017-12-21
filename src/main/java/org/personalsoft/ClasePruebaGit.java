696f89edd89e3343bca4d231bede4900fc806ba1
Daniel Serna <danielestebanse@gmail.com>
2017-12-21 11:53:13 -0500

    Realizando cambios en ClasePruebaGit

--- src/main/java/org/personalsoft/ClasePruebaGit.java
+++ src/main/java/org/personalsoft/ClasePruebaGit.java
@@ -2,4 +2,16 @@ package org.personalsoft;
 
 public class ClasePruebaGit {
 
+	private final int number;
+
+	public ClasePruebaGit(int number) {
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	
+	
+	
 }
